078eda03421a5fc2d9902cc8077f8d938719c813
IvanChebykin278 <deadkokos4@gmail.com>
2019-12-18 11:16:31 +0500

    first commit

---
+++ build/precache-manifest.9229a34b2ce2b1fe43c01fbdba07fb61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e0778c99882457972b429fe97ce9bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fca73e9bb811f026fb0d",
+    "url": "/static/css/main.1c154784.chunk.css"
+  },
+  {
+    "revision": "6597492bd3edeec044d8",
+    "url": "/static/js/2.279f584c.chunk.js"
+  },
+  {
+    "revision": "fca73e9bb811f026fb0d",
+    "url": "/static/js/main.200718c8.chunk.js"
+  },
+  {
+    "revision": "05fef59145ebdb7c05d6",
+    "url": "/static/js/runtime~main.c370128a.js"
+  }
+]);
